d6cfed2e29f35b0174f2d131e3841926908fe9b9
Victory Emmanuel <115421155+Victory-emmanuel@users.noreply.github.com>
2024-01-02 10:18:28 +0100

    Update search.js

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,16 +1,24 @@
+import styled from "@emotion/styled";
 import { SearchOutlined } from "@mui/icons-material";
+import { Box } from "@mui/material";
 const Search = ({ searched, setSearched, handleSearchedUser }) => {
   return (
     <>
-      <div id="search">
-        <form>
-          <input
+      <Box
+        sx={{
+          padding: "2rem",
+          marginTop: "1rem",
+        }}
+        id="search"
+      >
+        <SearchForm>
+          <SearchInput
             type="text"
             placeholder="Search..."
             value={searched}
             onChange={(e) => setSearched(e.target.value)}
           />
-          <button onClick={handleSearchedUser}>
+          <MyButton onClick={handleSearchedUser}>
             <SearchOutlined
               style={{
                 fontSize: "2rem",
@@ -18,11 +26,23 @@ const Search = ({ searched, setSearched, handleSearchedUser }) => {
                 background: "transparent",
               }}
             />
-          </button>
-        </form>
-      </div>
+          </MyButton>
+        </SearchForm>
+      </Box>
     </>
   );
 };
+const SearchForm = styled("form")({
+  display: "grid",
+  width: "100%",
+  alignItems: "center",
+  gridTemplateColumns: "90% 10%",
+});
+const SearchInput = styled("input")({ padding: "1rem", borderRadius: "15px" });
+const MyButton = styled("button")({
+  background: "none",
+  border: "none",
+  color: "#fff",
+});
 
 export default Search;
